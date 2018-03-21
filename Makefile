f74b5bdd35371baba8eb60d24838765d6182cd15
Albert Wolant <albertwolant@gmail.com>
2018-03-21 09:58:49 +0100

    Ported ip benchmark from legacy code

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ NVCC_FLAGS=-rdc=true -gencode arch=compute_61,code=sm_61 -std=c++11 -O3  -I $(LI
 BIN=$(BIN_DIR) $(BIN_DIR)/tmp
 LIB=$(BIN_DIR)/tmp/device_properties.o
 FLAVORS=$(BIN_DIR)/tmp/configuration.o $(BIN_DIR)/tmp/keys.o $(BIN_DIR)/tmp/masks.o $(BIN_DIR)/tmp/tree.o $(BIN_DIR)/tmp/utils.o $(BIN_DIR)/tmp/dataInfo.o
-BENCHMARKS=$(BIN_DIR)/tmp/benchmark.o $(BIN_DIR)/tmp/dictionary.o $(BIN_DIR)/tmp/words.o $(BIN_DIR)/tmp/runMain.o
+BENCHMARKS=$(BIN_DIR)/tmp/benchmark.o $(BIN_DIR)/tmp/dictionary.o $(BIN_DIR)/tmp/words.o $(BIN_DIR)/tmp/ip.o  $(BIN_DIR)/tmp/runMain.o
 
 all: lib flavors benchmarks
 
@@ -53,10 +53,13 @@ $(BIN_DIR)/tmp/dictionary.o: $(BENCH_SRC)/dictionary.cpp $(BENCH_SRC)/dictionary
 $(BIN_DIR)/tmp/words.o: $(BENCH_SRC)/words.cpp $(BENCH_SRC)/words.h
 	$(NVCC) $(NVCC_FLAGS) -c $(BENCH_SRC)/words.cpp -o $(BIN_DIR)/tmp/words.o
 
+$(BIN_DIR)/tmp/ip.o: $(BENCH_SRC)/ip.cpp $(BENCH_SRC)/ip.h
+	$(NVCC) $(NVCC_FLAGS) -c $(BENCH_SRC)/ip.cpp -o $(BIN_DIR)/tmp/ip.o
+
 $(BIN_DIR)/tmp/runMain.o: benchmark/benchmark-run/src/runMain.cpp
 	$(NVCC) $(NVCC_FLAGS) -c benchmark/benchmark-run/src/runMain.cpp -o $(BIN_DIR)/tmp/runMain.o
 
-$(BIN_DIR)/flavors-benchmarks: $(BENCH_SRC)/hostBenchmark.h $(BENCH_SRC)/randomBenchmark.h
+$(BIN_DIR)/flavors-benchmarks: $(BENCH_SRC)/hostBenchmark.h $(BENCH_SRC)/randomBenchmark.h $(BENCHMARKS)
 	$(NVCC) $(NVCC_FLAGS) -o $(BIN_DIR)/flavors-benchmarks $(BIN_DIR)/flavors.a $(BENCHMARKS)
 
 $(BIN_DIR):
---
+++ benchmark/flavors-benchmark/src/ip.cpp
@@ -0,0 +1,125 @@
+#include "ip.h"
+#include <sstream>
+
+using namespace Flavors;
+
+namespace FlavorsBenchmarks
+{
+    void parseLine(const std::string& line, std::vector<std::string>& tmpMasks, std::vector<unsigned>& lenghts)
+	{
+		std::vector<std::string> tokens;
+		std::string token;
+		std::stringstream streamLine{ line };
+
+		while (getline(streamLine, token, ';'))
+			tokens.push_back(token);
+
+		tmpMasks.push_back(tokens[2]);
+		try 
+        { 
+            lenghts.push_back(stoul(tokens[3])); 
+        }
+		catch (...) 
+        { 
+            //TODO: Exception 
+        }
+	}
+
+    unsigned parseTmpMask(std::string& tmpMask)
+    {
+        unsigned mask = 0;
+        std::vector<std::string> tokens;
+		std::string token;
+		std::stringstream streamLine{ tmpMask };
+
+		while (getline(streamLine, token, '.'))
+			tokens.push_back(token);
+
+		mask |= stoul(tokens[0]);
+		mask |= stoul(tokens[1]) << 8;
+		mask |= stoul(tokens[2]) << 16;
+		mask |= stoul(tokens[3]) << 24;
+
+        return mask;
+    }
+
+    Flavors::Masks IpBenchmark::loadIpSet(std::string& path)
+    {
+        std::vector<std::string> lines;
+		std::ifstream file;
+		file.open(path);
+
+		std::string line;
+
+        std::vector<std::string> tmpMasks;
+        std::vector<unsigned> lenghts;
+
+		if (file.good())
+		{
+			while (file >> line)
+                parseLine(line, tmpMasks, lenghts);
+		}
+
+        std::vector<unsigned> reorderedMasks(tmpMasks.size());
+        std::transform(tmpMasks.begin(), tmpMasks.end(), reorderedMasks.begin(), parseTmpMask);
+
+        Flavors::Masks masks(Flavors::Configuration::Default32, tmpMasks.size());
+
+        cuda::memory::copy(masks.Lengths.Get(), lenghts.data(), masks.Count * sizeof(unsigned));
+        cuda::memory::copy(masks.Store.Get(), reorderedMasks.data(), masks.Count * sizeof(unsigned));
+
+        return masks;
+    }
+
+    void IpBenchmark::runForDictionary(std::string& path)
+	{
+		std::cout << "Processing ip set: " << path << std::endl;
+		measured.Add("Dictionary", path);
+
+		try
+		{
+			timer.Start();
+			auto ipSet = loadIpSet(path);
+			measured.Add("Load", timer.Stop());
+
+			timer.Start();
+			ipSet.Sort();
+			measured.Add("Sort", timer.Stop());
+
+			CudaArray<unsigned> result{ ipSet.Count };
+
+			for(auto& config : configs)
+			{
+				measured.Add("Config", config);
+
+				timer.Start();
+				auto reshapedIpSet = ipSet.ReshapeMasks(config);
+				measured.Add("Reshape", timer.Stop());
+
+				timer.Start();
+				Tree tree{ reshapedIpSet };
+				measured.Add("Build", timer.Stop());
+				measured.Add("TreeMemory", tree.MemoryFootprint());
+				measured.Add("TreeLevels", tree);
+				measured.Add("Depth", tree.Depth());
+
+				timer.Start();
+				tree.Find(reshapedIpSet, result.Get());
+				measured.Add("Find", timer.Stop());
+                // measured.AddHitCount(result);
+
+                timer.Start();
+                tree.Match(reshapedIpSet, result.Get());
+                measured.Add("Match", timer.Stop());
+                measured.AddHitCount(result);
+            
+			    measured.AppendToFile(resultFile);
+            }
+        }
+		catch(...)
+		{ 
+			return; 
+		}
+	}
+
+}
