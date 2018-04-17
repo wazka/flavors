8f2a7368cd77625b891b5c16eb53cedf2acc1e15
Albert Wolant <albertwolant@gmail.com>
2018-04-17 18:37:13 +0200

    Tree tests refactored

--- test/helpers.h
+++ test/helpers.h
@@ -6,6 +6,8 @@
 #include "masks.h"
 #include "tree.h"
 
+#include "catch.hpp"
+
 using namespace Flavors;
 
 bool CheckSort(Keys& keys);
@@ -34,3 +36,60 @@ bool AllMasksInTree(Tree& tree, Masks& masks);
 bool CheckMasksFindResult(CudaArray<unsigned>& result, Masks& masks);
 
 bool CheckMatchResult(CudaArray<unsigned>& result, Masks& masks);
+
+template<class TreeType>
+void TreeFromKeysTest()
+{
+    //given
+    int count = 4;
+    auto data = vector<unsigned>{
+        1, 99, 50, 50,
+        5,  6,  9,  8,
+        9, 10, 11, 12};
+
+    Configuration config{vector<unsigned>{8, 8, 4}};
+
+    Keys keys{config, count};
+    keys.FillFromVector(data);
+
+    //when
+    TreeType tree{keys};
+
+    //then
+    REQUIRE(tree.Count == count);
+    REQUIRE(tree.Depth() == config.Depth());
+    REQUIRE(tree.Config == config);
+    REQUIRE(tree.h_LevelsSizes[0] == 1);
+    REQUIRE(tree.h_LevelsSizes[1] == 3);
+    REQUIRE(tree.h_LevelsSizes[2] == 4);
+
+    //given
+    CudaArray<unsigned> result{count};
+
+    //when
+    tree.Find(keys, result.Get());
+
+    //then
+    //0 in result would mean not found
+    //TODO: Tree constructor sorts keys under the hood
+    auto h_result = result.ToHost();
+    REQUIRE(h_result[0] == 1);
+    REQUIRE(h_result[1] == 4);
+    REQUIRE(h_result[2] == 3);
+    REQUIRE(h_result[3] == 2);
+
+    //given
+    Keys otherKeys{config, count};
+    otherKeys.FillFromVector(data);
+    result.Clear();
+
+    //when
+    tree.Find(otherKeys, result.Get());
+
+    //then
+    h_result = result.ToHost();
+    REQUIRE(h_result[0] == 1);
+    REQUIRE(h_result[1] == 2);
+    REQUIRE(h_result[2] == 3);
+    REQUIRE(h_result[3] == 4);
+}
